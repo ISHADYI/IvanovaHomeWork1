b89c7c880bc4735854f09040e0f80818c277acc6
ISHADYI <shadyworkq@gmail.com>
2026-03-15 20:53:52 +0300

    Решение задания №4

---
+++ WebApplicationStart/Controllers/CalcController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationStart.Controllers
+{
+    public class CalcController : Controller
+    {
+        public string Index(double a, double b, string c = "+")
+        {
+            switch (c)
+            {
+                case "+":
+                    return $"{a} + {b} = {a + b}";
+                case "-":
+                    return $"{a} - {b} = {a - b}";
+                case "*":
+                    return $"{a} * {b} = {a * b}";
+                case "/":
+                    if (b == 0)
+                        return "На ноль делить нельзя";
+                    else
+                        return $"{a} : {b} = {a / b}";
+                default:
+                    return $"{0} + {0} = {0 + 0}";
+            }
+        }
+    }
+}
