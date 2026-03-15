f8aed63604fb7d53dbc89a2489bcc0041d3da0e6
ISHADYI <shadyworkq@gmail.com>
2026-03-15 20:11:43 +0300

    Решение домашнего задания №1

---
+++ WebApplicationStart/Controllers/StartController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationStart.Controllers
+{
+    public class StartController : Controller
+    {
+        public string Hello()
+        {
+            int hour = DateTime.Now.Hour;
+
+            if (hour >= 0 && hour < 6)
+                return "Доброй ночи";
+            else if (hour >= 6 && hour < 12)
+                return "Доброе утро";
+            else if (hour >= 12 && hour < 18)
+                return "Добрый день";
+            else
+                return "Добрый вечер";
+        }
+    }
+}
