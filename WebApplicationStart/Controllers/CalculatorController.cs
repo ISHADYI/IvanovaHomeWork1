3423a65478eb795c60473e3f9c86c75ffbfdfaa1
ISHADYI <shadyworkq@gmail.com>
2026-03-15 20:37:44 +0300

    Решение домашнего задания №3

---
+++ WebApplicationStart/Controllers/CalculatorController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationStart.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index(double a, double b, string operation)
+        {
+            if (operation == "+")
+                return $"{a} + {b} = {a + b}";
+            else if (operation == "-")
+                return $"{a} - {b} = {a - b}";
+            else if (operation == "*")
+                return $"{a} * {b} = {a * b}";
+            else
+                return $"{a} + {b} = {a + b}";
+        }
+    }
+}
