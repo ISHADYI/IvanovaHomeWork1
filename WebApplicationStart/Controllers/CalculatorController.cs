d5d6aca32df653ac252a6e709f6513fac86537c7
ISHADYI <shadyworkq@gmail.com>
2026-03-15 20:27:04 +0300

    Решение домашнего задания №2

---
+++ WebApplicationStart/Controllers/CalculatorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationStart.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index(double a,  double b)
+        {
+            return $"{a} + {b} = {a + b}";
+        }
+    }
+}
