04d9eceb9459dfcb9892be5bdeeed50cc5be41d5
ISHADYI <shadyworkq@gmail.com>
2026-03-16 23:16:55 +0300

    Обозначила, что a,b = 0

--- WebApplicationStart/Controllers/CalculatorController.cs
+++ WebApplicationStart/Controllers/CalculatorController.cs
@@ -4,7 +4,7 @@ namespace WebApplicationStart.Controllers
 {
     public class CalculatorController : Controller
     {
-        public string Index(double a, double b, string operation)
+        public string Index(double a = 0, double b = 0, string operation = "+")
         {
             if (operation == "+")
                 return $"{a} + {b} = {a + b}";
